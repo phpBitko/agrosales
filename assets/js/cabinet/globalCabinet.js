5bb62a3f9823488ceb4f90b6fbbd99e10a96396f
bitko <bitko@dzk.gov.ua>
2018-10-31 19:04:07 +0200

    Add AdminLTE

--- assets/js/cabinet/globalCabinet.js
+++ assets/js/cabinet/globalCabinet.js
@@ -4,5 +4,6 @@ require ('@fortawesome/fontawesome');
 require ('@fortawesome/fontawesome-free-solid');
 require ('@fortawesome/fontawesome-free-brands');
 require ('@fortawesome/fontawesome-free-regular');
+require('../../js/cabinet/adminlte.min.js');
 
 require('../../css/cabinet/cabinet.scss');
