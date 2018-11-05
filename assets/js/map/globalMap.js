b38331e420739ad94f6b1f7155a43fb6014ca257
bitko <bitko@dzk.gov.ua>
2018-11-05 15:20:52 +0200

    fix design

--- assets/js/map/globalMap.js
+++ assets/js/map/globalMap.js
@@ -1,9 +1,9 @@
 bootbox = require('bootbox');
 
-require ('@fortawesome/fontawesome');
+/*require ('@fortawesome/fontawesome');
 require ('@fortawesome/fontawesome-free-solid');
 require ('@fortawesome/fontawesome-free-brands');
-require ('@fortawesome/fontawesome-free-regular');
+require ('@fortawesome/fontawesome-free-regular');*/
 
 require('../lib/jQueryPreloader/src/js/jquery.preloader.min');
 require('../lib/jQueryPreloader/src/css/preloader.css');
