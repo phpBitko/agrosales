a3efcba74cd93fde4506283edcaf072ad72f2ce2
bitko <bitko@dzk.gov.ua>
2018-11-05 14:54:56 +0200

    Fixed design.

--- assets/js/cabinet/globalCabinet.js
+++ assets/js/cabinet/globalCabinet.js
@@ -1,9 +1,10 @@
 bootbox = require('bootbox');
 
-require ('@fortawesome/fontawesome');
+/*require ('@fortawesome/fontawesome');
 require ('@fortawesome/fontawesome-free-solid');
 require ('@fortawesome/fontawesome-free-brands');
-require ('@fortawesome/fontawesome-free-regular');
+require ('@fortawesome/fontawesome-free-regular');*/
+
 require('../../js/cabinet/adminlte.js');
 
 require('../../css/cabinet/cabinet.scss');
