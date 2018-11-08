7c58e0aa2f33a1adaac97ad0e902e0c91be92baa
vetal33 <v.bitko@gmail.com>
2018-11-08 16:37:34 +0200

    add details advertisement page

--- src/AppBundle/Controller/AdvertisementController.php
+++ src/AppBundle/Controller/AdvertisementController.php
@@ -9,7 +9,10 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
-
+/**
+ * Class AdvertisementController
+ *
+ */
 
 class AdvertisementController extends Controller
 {
@@ -41,9 +44,31 @@ class AdvertisementController extends Controller
             $em = $this->getDoctrine()->getManager();
             $advertisementPoints = $em->getRepository('AppBundle:Advertisement')->selectPoint();
             return $this->json(array('data' => $advertisementPoints), Response::HTTP_OK);
-        }catch (\Exception $exception){
+        } catch (\Exception $exception) {
             return $this->json(array('error' => $exception->getMessage()), Response::HTTP_INTERNAL_SERVER_ERROR);
         }
+    }
+
+    /**
+     * @Route("/details/{id}",requirements={"id": "[1-9]\d*"},name="advertisement_details", methods={"GET"})
+     *
+     */
+    public function advertisementDetailsAction(Request $request,$id)
+    {
+        try {
+            $em=$this->getDoctrine()->getManager();
+            $advertisementDetails = $em->getRepository('AppBundle:Advertisement')->findOneBy(array('id'=>$id));
+            dump($advertisementDetails);
+            return $this->render('AppBundle:advertisement:details.html.twig', array('advertisement' => $advertisementDetails));
+//            if ($advertisementDetails->isActive()== true) {
+//                return $this->render('AppBundle:advertisement:details.html.twig', array('advertisement' => $advertisementDetails));
+//            } else {
+//                return $this->redirectToRoute('get_all_advertisement');
+//            }
+        }catch (\Exception $exception){
+
+        }
+
     }
 
 
