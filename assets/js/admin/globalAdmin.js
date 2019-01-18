9da43bfc4eae2cb96f776557d1f13fb3cfb472e5
bitko <bitko@dzk.gov.ua>
2019-01-18 14:15:17 +0200

    Add js

---
+++ assets/js/admin/globalAdmin.js
@@ -0,0 +1,15 @@
+bootbox = require('bootbox');
+
+/*require ('@fortawesome/fontawesome');
+require ('@fortawesome/fontawesome-free-solid');
+require ('@fortawesome/fontawesome-free-brands');
+require ('@fortawesome/fontawesome-free-regular');*/
+require('../lib/jQueryPreloader/src/js/jquery.preloader.min');
+require('../lib/jQueryPreloader/src/css/preloader.css');
+
+require('../../js/lib/adminLTE/js/adminlte');
+
+bootboxMessage = require('../../js/functionLib/bootboxMessage');
+bootboxAlertMessage = require('../../js/functionLib/bootboxAlertMessage');
+
+require('../../css/admin/admin.scss');
