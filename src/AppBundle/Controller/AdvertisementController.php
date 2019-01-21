f3414e53677a301283ffb5661f3fc9218f3815a2
vetal33 <v.bitko@gmail.com>
2019-01-21 17:28:10 +0200

    fix Sorting

--- src/AppBundle/Controller/AdvertisementController.php
+++ src/AppBundle/Controller/AdvertisementController.php
@@ -26,9 +26,8 @@ class AdvertisementController extends SuperController
      * @Route("/{typeView}",  defaults={"page": 1}, requirements={"typeView": "list|tab"}, name="advertisement_index", methods={"GET"})
      * @return Response
      */
-    public function indexAction(Request $request, $typeView = 'list')
+    public function indexAction(Request $request, PaginatorServices $paginator, $typeView = 'list')
     {
-
         $adv = new Advertisement();
         $em = $this->getDoctrine()->getManager();
         $purpose = $em->getRepository('AppBundle:DirPurpose')->findAll();
@@ -53,14 +52,14 @@ class AdvertisementController extends SuperController
             $query = $advertisement->queryFindByStatus(self::STATUS_ADVERTISEMENT['ACTIVE'], Advertisement::$order);
         }
 
-        $paginator = $this->get('app.service.paginator_services');
         $pagination = $paginator->getPagination($query, $request->query->getInt('page', 1));
-        dump($paginator);
+        $stringSelection = $this->identifySelectString($request);
 
         return $this->render('AppBundle:advertisement:index.html.twig', array(
-            'form' => $form->createView(),
-            'advertisement' => $pagination,
-            'typeView' => $typeView,
+            'form'            => $form->createView(),
+            'advertisement'   => $pagination,
+            'typeView'        => $typeView,
+            'stringSelection' => $stringSelection,
 
         ));
     }
@@ -118,5 +117,33 @@ class AdvertisementController extends SuperController
         $this->addFlash('success', 'Оголошення активовано.');
         return $this->redirectToRoute('advertisement_details', ['id' => $advertisement->getId()]);
     }
+
+
+    /**
+     * Формує строку для сортування
+     *
+     * @param Request $request
+     * @return string
+     */
+    protected function identifySelectString(Request $request)
+    {
+        $stringSelected = 'Сортувати';
+        if($request->query->get('sort')) {
+            $field = explode('.',$request->query->get('sort'),2);
+            $ordertype = $request->query->get('direction');
+            if($field[1] == 'price'){
+                $addString = ($ordertype =='asc')? ' дешевші': ' дорожчі';
+                $stringSelected = "Спочатку $addString";
+            } elseif ($field[1] == 'area'){
+                $addString = ($ordertype =='asc')? ' менші': ' більші';
+                $stringSelected = "Спочатку $addString за площею";
+            } elseif ($field[1] == 'addDate'){
+                $addString = ($ordertype =='asc')? ' раніше': ' пізніше';
+                $stringSelected = "Спочатку $addString додані";
+            }
+        }
+        return $stringSelected;
+    }
+
 
 }
